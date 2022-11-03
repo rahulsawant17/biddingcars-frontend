570c07b1b5ecd782f7353d6bfc700a3b70ca675e
rahul <rahulsawant17396@gmail.com>
2022-11-03 21:43:48 +0530

    endpoint changes

--- helpers/urlConfig.js
+++ helpers/urlConfig.js
@@ -1,2 +1,2 @@
-export const api = 'http://bidbackend/.netlify/functions/server/';
+export const api = 'https://bidbackend.netlify.app/.netlify/functions/server';
 // export const api = 'https://biddingcarsserver.netlify.app/.netlify/functions/server/';
